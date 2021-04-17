5b3bd70589c03f9493501cbfb647645ad5f5b2b9
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-04-17 00:15:19 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Follow along with the examples here
+
+function sayHelloTo(firstName) {console.log(`Hello, ${firstName}!`);}
+
+sayHelloTo('Mr. Stark');
+sayHelloTo('Captain Rogers');
+sayHelloTo('Agent Romanov');
+
+function say(greeting, firstName) {console.log(`${greeting}, ${firstName}!`);}
+say("Wanda", "Welcome home")
+
+function say(greeting, firstName) {
+    console.log('I am Captain America');
+    return `${greeting}, ${firstName}!`;
+}
+    console.log(say("John", "Walker"));  
